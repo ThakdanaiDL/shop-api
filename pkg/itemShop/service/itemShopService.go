27d69800a994cb591a5dbfcbd9377ba32a4966e3
DESKTOP-786QVJ1\guitar <thakdanai.limthongkum@gmail.com>
2025-05-27 00:36:14 +0700

    "ทำ listing endpoint ที่ service "

--- pkg/itemShop/service/itemShopService.go
+++ pkg/itemShop/service/itemShopService.go
@@ -1,4 +1,9 @@
 package services
 
+import (
+	_itemshopModel "github.com/ThakdanaiDL.git/shop-api/pkg/itemShop/model"
+)
+
 type ItemShopService interface {
+	Listing() ([]*_itemshopModel.Item, error) // layer service จะใช้ model ของ itemShopเเทน entities
 }
--- pkg/itemShop/service/itemShopServiceImpl.go
+++ pkg/itemShop/service/itemShopServiceImpl.go
@@ -1,13 +1,35 @@
 package services
 
 import (
-	_itemShopRepository "github.com/ThakdanaiDL.git/shop-api/pkg/itemShop/repository"
+	_itemshopModel "github.com/ThakdanaiDL.git/shop-api/pkg/itemShop/model"
+	"github.com/ThakdanaiDL.git/shop-api/pkg/itemShop/repository"
 )
 
 type itemShopServiceImp struct {
-	itemShopRepository _itemShopRepository.ItemShopRepository
+	itemShopRepository repository.ItemShopRepository
 }
 
-func NewItemShopRepositoryImpl(itemShopRepository _itemShopRepository.ItemShopRepository) ItemShopService {
-	return &itemShopServiceImp{itemShopRepository}
+func NewItemShopRepositoryImpl(itemShopRepository repository.ItemShopRepository) ItemShopService {
+
+	return &itemShopServiceImp{itemShopRepository: itemShopRepository}
+
+}
+
+func (s *itemShopServiceImp) Listing() ([]*_itemshopModel.Item, error) {
+
+	itemList, err := s.itemShopRepository.Listing() //ดึงรายการมาจาก repository
+	if err != nil {
+		return nil, err
+	}
+
+	itemModelList := make([]*_itemshopModel.Item, 0) // สร้าง sliceเปล่า เพื่อเก็บ item model
+
+	for _, item := range itemList {
+
+		itemModelList = append(itemModelList, item.ToItemShopModel()) // เเปลง item entity เป็น item model เเละเพิ่มเข้าไปใน slice
+
+	}
+
+	return itemModelList, nil
+
 }
