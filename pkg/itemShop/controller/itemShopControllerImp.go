76462f91037f411412984d75256b2540d6b01f94
DESKTOP-786QVJ1\guitar <thakdanai.limthongkum@gmail.com>
2025-05-27 00:36:43 +0700

    "ทำ listing endpoin ที่ controller "

--- pkg/itemShop/controller/itemShopControllerImp.go
+++ pkg/itemShop/controller/itemShopControllerImp.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"net/http"
+
 	_itemShopService "github.com/ThakdanaiDL.git/shop-api/pkg/itemShop/service"
+	"github.com/labstack/echo/v4"
 )
 
 type itemShopControllerImpl struct {
@@ -9,5 +12,15 @@ type itemShopControllerImpl struct {
 }
 
 func NewItemShopControllerImpl(itemShopService _itemShopService.ItemShopService) ItemShopController {
-	return &itemShopControllerImpl{itemShopService}
+	return &itemShopControllerImpl{itemShopService: itemShopService}
+}
+
+func (c *itemShopControllerImpl) Listing(pctx echo.Context) error {
+
+	itemModelList, err := c.itemShopService.Listing()
+	if err != nil {
+		return pctx.String(http.StatusInternalServerError, "Error fetching item list: "+err.Error())
+	}
+	return pctx.JSON(http.StatusOK, itemModelList) // Return the list of items as JSON response
+
 }
