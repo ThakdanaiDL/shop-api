ebfd2d88059b43423163e3e37adf1d2aa3498db3
DESKTOP-786QVJ1\guitar <thakdanai.limthongkum@gmail.com>
2025-05-27 00:35:43 +0700

    "ทำ listing endpoint ที่ repo "

--- pkg/itemShop/repository/itemShopRepository.go
+++ pkg/itemShop/repository/itemShopRepository.go
@@ -1,4 +1,7 @@
 package repository
 
+import "github.com/ThakdanaiDL.git/shop-api/entities"
+
 type ItemShopRepository interface {
+	Listing() ([]*entities.Item, error)
 }
--- pkg/itemShop/repository/itemShopRepositoryImp.go
+++ pkg/itemShop/repository/itemShopRepositoryImp.go
@@ -1,8 +1,29 @@
 package repository
 
+import (
+	"github.com/ThakdanaiDL.git/shop-api/entities"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
 type itemShopRepositoryImpl struct {
+	logger echo.Logger
+	db     *gorm.DB
 }
 
-func NewItemShopRepositoryImpl() ItemShopRepository {
-	return &itemShopRepositoryImpl{}
+func NewItemShopRepositoryImpl(db *gorm.DB, logger echo.Logger) ItemShopRepository {
+	return &itemShopRepositoryImpl{db: db, logger: logger}
+}
+
+func (r *itemShopRepositoryImpl) Listing() ([]*entities.Item, error) { // Listing retrieves a list of items from the database
+	itemList := make([]*entities.Item, 0) // Create a slice to hold the items
+
+	if err := r.db.Find(&itemList).Error; err != nil {
+		r.logger.Errorf(" Fail to Listing item :%s", err.Error())
+		return nil, err
+
+	}
+
+	return itemList, nil
+
 }
