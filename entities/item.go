1aa14ac0b808e5ac4f2f41b6801f4d78dac488c8
DESKTOP-786QVJ1\guitar <thakdanai.limthongkum@gmail.com>
2025-05-27 00:34:36 +0700

    "END POINT itemshop start  here  :: ทำ func convert entitie to model , สร้าง model ใน itemshop เพื่อใช้ ใน service layer :: "

--- entities/item.go
+++ entities/item.go
@@ -1,6 +1,10 @@
 package entities
 
-import "time"
+import (
+	"time"
+
+	_itemshopModel "github.com/ThakdanaiDL.git/shop-api/pkg/itemShop/model"
+)
 
 type Item struct {
 	ID          uint64    `gorm:"primaryKey;autoIncrement;"`         // ID of the item
@@ -14,14 +18,12 @@ type Item struct {
 	UpdatedAt   time.Time `gorm:"autoUpdateTime;not null;"` // ID of the admin who created the item
 }
 
-// type Item struct {
-// 	ID          uint64    `gorm:"primaryKey;autoIncrement"`
-// 	AdminID     *string   `gorm:"type:varchar(64);"`
-// 	Name        string    `gorm:"type:varchar(64);unique;not null;"`
-// 	Description string    `gorm:"type:varchar(128);not null;"`
-// 	Picture     string    `gorm:"type:varchar(256);not null;"`
-// 	Price       uint      `gorm:"not null;"`
-// 	IsArchive   bool      `gorm:"not null;default:false;"`
-// 	CreatedAt   time.Time `gorm:"not null;autoCreateTime;"`
-// 	UpdatedAt   time.Time `gorm:"not null;autoUpdateTime;"`
-// }
+func (i *Item) ToItemShopModel() *_itemshopModel.Item {
+	return &_itemshopModel.Item{
+		ID:          i.ID,
+		Name:        i.Name,
+		Description: i.Description,
+		Picture:     i.Picture,
+	}
+
+}
---
+++ pkg/itemShop/model/itemShop.go
@@ -0,0 +1,16 @@
+package model
+
+type (
+	Item struct {
+		ID          uint64 `json:"id"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		Picture     string `json:"picture"`
+		Price       uint   `json:"price"`
+
+		// AdminID     *string   `gorm:"type:varchar(64);"`        // ID of the admin who created the item client ไม่จำเป็นต้องรู้
+		// IsAchived   bool      `gorm:"default:false;not null;"`  // ID of the admin who created the item client ไม่จำเป็นต้องรู้
+		// CreatedAt   time.Time `gorm:"autoCreateTime;not null;"` // ID of the admin who created the item client ไม่จำเป็นต้องรู้
+		// UpdatedAt   time.Time `gorm:"autoUpdateTime;not null;"` // ID of the admin who created the item client ไม่จำเป็นต้องรู้
+	}
+)
